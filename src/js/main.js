214275364016b8e5dc86d71ef507fbeb71528398
bartlomiejw <wariasbj@gmail.com>
2021-05-08 12:45:35 +0200

    instrukcje warunkowe / pętle

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,30 @@
 
 
 console.log(`Hello world!`);
+
+// let licznik = 0;
+// let linia = "";
+// while (licznik <= 9) {
+//     linia = linia + "#";
+//     licznik ++;
+//     console.log(linia);
+// }
+
+//     let linia = "";
+// for (let i = 0; i <= 9; i = i + 1) {
+
+//     linia = linia + "#"
+// console.log(linia);
+// }
+
+let licznik = 4;
+while (licznik == 4) {
+  let wynik = prompt("Jaki jest wynik działania: 2+2?");
+  if (wynik == 4) {
+
+    alert("PERFEKCJA");
+    break;
+  } else {
+    alert("CHUJÓWKA");
+  }
+}
